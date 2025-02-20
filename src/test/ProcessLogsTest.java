14146da6b7b4d60877e2488858dc9bf114350e9d
scja0 <s@h.com>
2025-02-21 02:45:25 +0700

    Refactor ConsoleLogger to be its own class so different packages can call it and remove bloat from main

---
+++ src/test/ProcessLogsTest.java
@@ -0,0 +1,14 @@
+package test;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.io.IOException;
+import java.time.Duration;
+import java.util.logging.Logger;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class ProcessLogsTest {
+
+}
